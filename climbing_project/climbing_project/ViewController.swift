332a56a29cc2f17dae686efe1118a86f0c4d10c4
Itaru7 <ir172178@gmail.com>
2018-12-27 14:42:50 -0800

    Initial xcode workspace

---
+++ climbing_project/climbing_project/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  climbing_project
+//
+//  Created by 岸川　至白 on 12/27/18.
+//  Copyright © 2018 Climbing Project. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
